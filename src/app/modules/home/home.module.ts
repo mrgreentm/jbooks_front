42033bf0886d27c876dd63b63bcc8cf4fe5d91ec
João Jacinto <joaonetotfm@gmail.com>
2022-05-12 22:01:03 -0300

    wip: modulo de postagens

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,12 +1,12 @@
+import { MatIconModule } from '@angular/material/icon';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
-import {MatCardModule} from '@angular/material/card';
-
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule,MatCardModule],
+  imports: [CommonModule, HomeRoutingModule, MatCardModule, MatIconModule],
 })
 export class HomeModule {}
